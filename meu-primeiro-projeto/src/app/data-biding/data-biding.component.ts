e0b2451aafd7d9a089a83a13ba5f2a0893a9589f
EsterMel0 <estefanymelo028@gmail.com>
2023-05-27 11:31:08 -0400

    conhecendo o Property Binding

--- meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
+++ meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
@@ -10,6 +10,10 @@ export class DataBidingComponent implements OnInit {
   public nome: string = "Dener";
   public idade: number = 29;
 
+  public checkedDisabled: boolean = false;
+
+  public imgSrc: string = "https://www.google.com/search?q=bolinho+de+lua&client=ubuntu-sn&hs=Zwz&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiisP6_5pX_AhUfr5UCHdjEA6EQ_AUoAXoECAMQAw&biw=1846&bih=968&dpr=1#imgrc=tKxo1MRLhJmdqM";
+
   constructor(){ }
 
   ngOnInit(): void {
