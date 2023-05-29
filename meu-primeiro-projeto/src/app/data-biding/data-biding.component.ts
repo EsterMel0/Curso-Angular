6129aba78a9de3d21a97e4d756644c012288cab7
EsterMel0 <estefanymelo028@gmail.com>
2023-05-29 15:06:47 -0400

    conhecendo o Event Binding

--- meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
+++ meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
@@ -12,7 +12,11 @@ export class DataBidingComponent implements OnInit {
 
   public checkedDisabled: boolean = false;
 
-  public imgSrc: string = "https://www.google.com/search?q=bolinho+de+lua&client=ubuntu-sn&hs=Zwz&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiisP6_5pX_AhUfr5UCHdjEA6EQ_AUoAXoECAMQAw&biw=1846&bih=968&dpr=1#imgrc=tKxo1MRLhJmdqM";
+  public imgSrc: string = "";
+
+  public alertaInfo(){
+    alert("Deu certo!")
+  }
 
   constructor(){ }
 
